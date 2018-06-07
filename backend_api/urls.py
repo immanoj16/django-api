30909772483e742f248449624c46b93797eb356c
Manoj Kumar Maharana <manoj@triveous.com>
2018-06-07 15:15:19 +0530

    Add: frontend urls and index page

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/users/', include(('users.urls', 'users'), namespace='users')),
     url(r'^api-auth/expenses/', include(('expenses.urls', 'expenses'), namespace='expenses')),
+    url(r'', include('frontend.urls'))
 ]
